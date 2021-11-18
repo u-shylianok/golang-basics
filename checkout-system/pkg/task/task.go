ffc050dfe92e7ca9883423c30abeeab871ff38cc
u-shylianok <u.shylianok@gmail.com>
2021-11-18 19:01:49 +0300

    Add DefaultCatalog and sum func of products price

--- checkout-system/pkg/task/task.go
+++ checkout-system/pkg/task/task.go
@@ -1,6 +1,42 @@
 package task
 
-func TaskWorkExample(SKUs []string) int64 {
+import "github.com/u-shylianok/golang-basics/checkout-system/pkg/model"
 
-	return int64(len(SKUs))
+func GetDefaultCatalog() model.Catalog {
+	return model.Catalog{
+		Products: map[string]model.Product{
+			"ipd": {
+				SKU:   "ipd",
+				Name:  "Super iPad",
+				Price: 54999,
+			},
+			"mbp": {
+				SKU:   "mbp",
+				Name:  "MacBook Pro",
+				Price: 139999,
+			},
+			"atv": {
+				SKU:   "atv",
+				Name:  "Apple TV",
+				Price: 10950,
+			},
+			"vga": {
+				SKU:   "vga",
+				Name:  "VGA adapter",
+				Price: 3000,
+			},
+		},
+	}
+}
+
+func TaskWorkExample(SKUs []string) int64 {
+	catalog := GetDefaultCatalog()
+
+	var total int64
+
+	for _, SKU := range SKUs {
+		total += catalog.Products[SKU].Price
+	}
+
+	return total
 }
