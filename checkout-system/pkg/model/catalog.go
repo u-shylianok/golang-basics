c692eadcaa27aed3aff46649b4d1a899f0c8d744
u-shylianok <u.shylianok@gmail.com>
2021-11-18 19:08:54 +0300

    Replace DefaultCatalog to model package

--- checkout-system/pkg/model/catalog.go
+++ checkout-system/pkg/model/catalog.go
@@ -3,3 +3,30 @@ package model
 type Catalog struct {
 	Products map[string]Product
 }
+
+func GetDefaultCatalog() Catalog {
+	return Catalog{
+		Products: map[string]Product{
+			"ipd": {
+				SKU:   "ipd",
+				Name:  "Super iPad",
+				Price: 54999,
+			},
+			"mbp": {
+				SKU:   "mbp",
+				Name:  "MacBook Pro",
+				Price: 139999,
+			},
+			"atv": {
+				SKU:   "atv",
+				Name:  "Apple TV",
+				Price: 10950,
+			},
+			"vga": {
+				SKU:   "vga",
+				Name:  "VGA adapter",
+				Price: 3000,
+			},
+		},
+	}
+}
--- checkout-system/pkg/task/task.go
+++ checkout-system/pkg/task/task.go
@@ -2,35 +2,8 @@ package task
 
 import "github.com/u-shylianok/golang-basics/checkout-system/pkg/model"
 
-func GetDefaultCatalog() model.Catalog {
-	return model.Catalog{
-		Products: map[string]model.Product{
-			"ipd": {
-				SKU:   "ipd",
-				Name:  "Super iPad",
-				Price: 54999,
-			},
-			"mbp": {
-				SKU:   "mbp",
-				Name:  "MacBook Pro",
-				Price: 139999,
-			},
-			"atv": {
-				SKU:   "atv",
-				Name:  "Apple TV",
-				Price: 10950,
-			},
-			"vga": {
-				SKU:   "vga",
-				Name:  "VGA adapter",
-				Price: 3000,
-			},
-		},
-	}
-}
-
 func TaskWorkExample(SKUs []string) int64 {
-	catalog := GetDefaultCatalog()
+	catalog := model.GetDefaultCatalog()
 
 	var total int64
 
